9ece506ac2df5e54f10191c729554bf353ff7d95
juarodri <juan.martin.rodriguez.caillava@gmail.com>
2020-04-08 00:28:12 -0300

    Cambios Juan

---
+++ ThreadsCh/src/Server.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class Server 
+{
+	String nombre;
+	
+    public static void main( String[] args )
+    {
+    	// Cliente / Servidor  
+    	// 2 procesos, 1 proceso servidor (escuchando)
+    	// 1 proceso cliente (conn - Server)
+    	// 1 Server - TCP - Capa 4 --- IP: IP ; TCP : Puerto
+    	
+    	try {
+			// PASO 1 - Crear servidor (puerto)
+    		ServerSocket ss = new ServerSocket (9000);
+			System.out.println( " OK FUNCIONANDO en el peurto 9000");
+			
+			// Paso 2 (repetitivo)
+			while (true) {
+				// aceptar clientes
+				Socket cliente = ss.accept();
+				System.out.println(" CLIENTE RECIBIDO: "+cliente.getInetAddress().getCanonicalHostName() + " : " +cliente.getPort());
+				
+				// COMO HACER UN THREAD
+				// 1 - Instanciar la clase
+				//ServerHijo sh = new ServerHijo(cliente);
+				// 2 - Crear un thread y decirle que va a ejecutar con la instancia X
+				//Thread shThread = new Thread(sh);
+				// 3 - Ejecutar el hilo (start)
+				//shThread.start();
+				
+				BufferedReader canalEntrada = new BufferedReader (new InputStreamReader (cliente.getInputStream()));
+				PrintWriter canalSalida = new PrintWriter (cliente.getOutputStream(), true);
+				System.out.println("Por leer del socket");
+				String msg = canalEntrada.readLine();
+				System.out.println("Lei del socket");
+				canalSalida.println(msg);
+
+				System.out.println("2: Por leer del socket");
+				msg = canalEntrada.readLine();
+				System.out.println("2: Lei del socket");
+				canalSalida.println(msg);
+				
+				cliente.close();
+			}
+			
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.err.println( " PUERTO EN USO");
+			//e.printStackTrace();
+		} 
+    	
+      
+    }
+}
