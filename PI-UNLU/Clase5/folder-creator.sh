ad4f4a43d812b8eaee6cb01cb9bdda586936bd66
David Petrocelli <dmpetrocelli@gmail.com>
2020-10-23 20:52:36 -0300

    class 6 - finished

--- PI-UNLU/Clase5/folder-creator.sh
+++ PI-UNLU/Clase5/folder-creator.sh
@@ -1,1 +1,2 @@
 mkdir /tmp/rabbit; mkdir /tmp/rabbit/stats ; mkdir /tmp/rabbit/node1 ; mkdir /tmp/rabbit/node2 ; mkdir /tmp/rabbit/node3; sudo chmod 777 -R /tmp/rabbit
+docker-compose -f rabbit-prueba-compose.yaml up
